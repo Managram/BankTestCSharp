968f1998727eb07ecc424c62cadd08b60c241e3a
Patrick <p_donati@hotmail.com>
2016-05-18 11:02:25 +0000

    started setting up unit tests

---
+++ BankTechTest/BankTechTests/BankUnitTests.cs
@@ -0,0 +1,21 @@
+﻿using BankTechTest;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTechTests
+{
+    [TestClass]
+    public class BankUnitTests
+    {
+        [TestMethod]
+        public void Balance()
+        {
+            Account account = new Account();
+            double expected = 0;
+
+            account.Balance();
+
+
+        }
+    }
+}
