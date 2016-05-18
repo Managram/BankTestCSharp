08f0f4e7877ef721d835717e551482dab755d77a
Patrick <p_donati@hotmail.com>
2016-05-18 16:15:51 +0000

    working on output for statement

--- BankTechTest/BankTechTest/BankTechTest.cs
+++ BankTechTest/BankTechTest/BankTechTest.cs
@@ -8,12 +8,22 @@ namespace BankTechTest
 {
     public class Account
     {
-        public decimal balance;
-        public const int default_balance = 0;
+        private decimal balance;
+        private string history;
+        private const int default_balance = 0;
+        private const string history_header = "date || credit || debit || balance";
+        DateTime dateAndTime = DateTime.Now;
+        private string date = dateAndTime.ToString("dd/MM/yyyy"));
 
         public Account()
         {
             balance = default_balance;
+            history = history_header;
+        }
+
+        public string History()
+        {
+            return history;
         }
 
         public decimal Balance()
@@ -24,6 +34,7 @@ namespace BankTechTest
         public void Credit(decimal amount)
         {
             balance += amount;
+            history += "" + date + "||" + amount + "|| ||" + balance + "\n"; ;
         }
 
         public void Debit(decimal amount)
--- BankTechTest/BankTechTests/BankUnitTests.cs
+++ BankTechTest/BankTechTests/BankUnitTests.cs
@@ -8,12 +8,24 @@ namespace BankTechTests
     public class BankUnitTests
     {
         [TestMethod]
-        public void Account_initializesWithBalanceOfZero()
+        public void Account_initializesWith_BalanceOfZero()
         {
             Account account = new Account();
             decimal expected = 0;
 
             decimal actual = account.Balance();
+
+            Assert.AreEqual(expected, actual);
+
+
+        }
+        [TestMethod]
+        public void Account_initializesWith_BlankTransactionHistory()
+        {
+            Account account = new Account();
+            string expected = "date || credit || debit || balance";
+
+            string actual = account.History();
 
             Assert.AreEqual(expected, actual);
 
@@ -28,11 +40,23 @@ namespace BankTechTests
 
             account.Credit(creditAmount);
 
-            decimal actual = account.balance;
+            decimal actual = account.Balance();
 
             Assert.AreEqual(expected, actual);
 
 
+        }
+        [TestMethod]
+        public void Credit_RecordsTransactionInHistory()
+        {
+            Account account = new Account();
+            decimal creditAmount = 10;
+            string expected = "{date} || {amount} || || {balance}";
+
+            account.Credit(creditAmount);
+
+            string actual = account.History();
+            Assert.AreEqual(expected, actual);
         }
         [TestMethod]
         public void Debit_RemovesAmountFromBalance()
@@ -45,7 +69,7 @@ namespace BankTechTests
             account.Credit(creditAmount);
             account.Debit(debitAmount);
 
-            decimal actual = account.balance;
+            decimal actual = account.Balance();
 
             Assert.AreEqual(expected, actual);
         }
